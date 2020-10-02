48d6dd55f191da1d28244f5b6c02c5222f996cef
Robert Greenawalt <robert.greenawalt29@gmail.com>
2020-10-01 23:53:01 -0400

    moved all the get and post requests to api_routes file; deleted an extraneous app.listen; modified the fs.readfile on the api_routes page so that it properly matched up with the location of the db.json file

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -5,15 +5,55 @@ const path = require("path");
 
 
 
-fs.readFile("db/db.json", "utf8", function (err, data) {
-    if (err) throw err;
+module.exports = function (app) {
+
+    fs.readFile("Develop/db/db.json", "utf8", function (err, data) {
+        if (err) throw err;
+
+        //making the contents of the file (the data) into json format
+        const notes = JSON.parse(data)
+        console.log(notes);
 
 
 
+        //API get requests
+        app.get("/api/notes", function(req,res) {  
+            res.json(notes);
+        })
+
+        
+
+
+        //API post request
+        //allowing user to create a note; this posts the note
+        app.post("/api/notes", function (req, res) {
+            let newNote = req.body;
+
+            console.log(newNote);
+
+            notes.push(newNote);
+
+            console.log("newNote added");
+            
+        });
+
+
+        //notes with id
+        app.get("/api/notes/:id", function (req, res) {
+            res.json(notes[req.params.id]);
+        });
+
+
+        // app.delete("/api/notes/:id", function (req, res) {
+        // still working on this    
+        // })
+
+    });
 
 
 
-    
-});
+}
+
+
 
 
--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-
 // // Dependencies
 // // =============================================================
 var express = require("express");
-var path = require("path");
 const path = require("path");
 
 
-
-// // Sets up the Express App
+// // Sets up the Express server
 // =============================================================
 var app = express();
 var PORT = 6000;
+
 
 // // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -19,28 +17,19 @@ app.use(express.json());
 
 
 
-// const db = require("../db/db.json")
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
 
+require("./routes/api_routes")(app);
+require("./routes/html_routes")(app);
 
-
-// // // taking user to the notes page once they click get started
-app.get("/api/notes", function (req, res) {
-
-    res.json(notes);
-});
-
-// console.log("***were here**")
-
-
-
-
-
-
-
-
-
-// // Starts the server to begin listening
-// // =============================================================
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
